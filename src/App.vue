6c233d7cb60937aedfdc7d6b8ff8d0ce4a276d31
valeriofilareti <valerio.filareti@gmail.com>
2023-04-26 15:42:33 +0200

    [add] print title

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
 <script>
 
   export default {
-    
+    name: 'App',
+    data(){
+      return {
+        title: 'vite-app'
+      }
+    }
   }
-  
+
 </script>
 
 <template>
-  
+  <h1>{{ title }}</h1>
 </template>
 
 <style>
